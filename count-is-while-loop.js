920d1a79a48b90a62928033c403000da77ab425d
Jatin Kapur <jkapur.work@gmail.com>
2019-09-13 11:01:46 -0500

    initial commit, build on the while loop

---
+++ count-is-while-loop.js
@@ -0,0 +1,101 @@
+/*
+var count = 5; 
+
+while(count <= 20) { 
+    console.log("count is: " + count); 
+count+=2; 
+}
+
+
+var str =  "hello"; 
+
+var count = 0; 
+
+while(count < str.length) { 
+    console.log(str[count]); 
+    count+=2; 
+}
+
+
+var num = 1; 
+while( num <= 10) { 
+    console.log(num); 
+    num +=2; 
+}
+
+
+var num = 1 
+
+while(num <= 20) { 
+    if(num % 4 === 1){ 
+        console.log(num); 
+    }
+    if (num % 4 === 0) { 
+        console.log(num); 
+    }
+    if (num % 4 === 2) { 
+        console.log(num); 
+    }
+    if (num % 4 === 3) { 
+        console.log(num); 
+    }
+    num++; 
+}
+*/
+/*
+var num = 1 
+
+while(num <= 20) { 
+    
+    if (num % 4 === 0) { 
+        console.log(num); 
+    
+    num++; 
+}
+*/
+
+var num = 149; 
+
+while(num < 150 && num > 80) { 
+    console.log(num + 1); 
+    num --; 
+}
+
+
+
+
+// print all numbers between -10 and 19 
+/*
+let num = -10; 
+
+while(num >= -10 && num < 19 ) { 
+    console.log(num); 
+    num++; 
+}
+*/
+
+//print all even numbers between 10 and 40 
+
+let num = 10; 
+
+while(num >= 10 && num <= 40 ) { 
+    console.log(num); 
+    num+=2; 
+}
+
+
+
+//print all odd numbers between 300 and 333
+var num = 300; 
+
+while (num >= 300 && num <= 333) {
+    console.log(num); 
+    num+=2; 
+} 
+
+
+
+/*
+oddd numbers 301	303	305	307	309	311	313	315	317	319
+321	323	325	327	329	331	333
+*/
